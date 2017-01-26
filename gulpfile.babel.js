1ca2aba6fbd4b3f3dfc71b9cd275702e57f88afb
uto_usui <usui@re-vite.com>
2017-01-26 13:08:19 +0900

    [update] ES6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,89 +1,117 @@
-//
-//start
-let gulp = require('gulp');
+/**
+ * Create paths and import packages
+ *
+ * いろんなところにパスを通す。
+ * パッケージを読み込む。
+ *
+ */
+const gulp = require('gulp'),
 
-let docs = '.';
-//
-let distDir =  docs + '/dist';
-let srcDir =  docs + '/src';
-//
-let assetsDir = srcDir + '/assets';
-let DistAssetsDir = distDir + '/assets';
-//
-let path = {
-  'imgPath': assetsDir + '/images',
-  'sassPath': assetsDir + '/sass',
-  'cssPath': assetsDir + '/css',
-  'jsPath': assetsDir + '/js'
-};
-//
-let distPath = {
-  'imgPath': DistAssetsDir + '/images',
-  'sassPath': DistAssetsDir + '/sass',
-  'cssPath': DistAssetsDir + '/css',
-  'jsPath': DistAssetsDir + '/js'
-};
-//
-// plugin
-//
-let plumber = require('gulp-plumber'); // error escape
-let rename = require('gulp-rename'); // rename
-let sourcemaps = require('gulp-sourcemaps'); // sourcemap
-let gulpSequence = require('gulp-sequence'); // sequence
-//
-let autoprefixer = require('gulp-autoprefixer'); // prefix
-let sass = require('gulp-compass'); // Sass compass
-let csscomb = require('gulp-csscomb'); // css
-let cssmin = require('gulp-cssmin'); // css min
-let frontnote = require('gulp-frontnote'); // style guide
-//
-let uglify = require('gulp-uglify'); // js min
-let babel = require('gulp-babel'); // es6
-let concat = require('gulp-concat'); // concat ... order.JSON
-let eslint = require('gulp-eslint'); // eslint
-//
-let ejs = require('gulp-ejs'); // ejs template
-let minifyHtml = require('gulp-minify-html'); // html min
-//
-let browser = require('browser-sync'); // browser start
-//
-let imagemin = require('gulp-imagemin'); // image min
-let pngquant = require('imagemin-pngquant');
-//
-let notify = require('gulp-notify'); // alert
-let watch = require("gulp-watch");  // watch
-//
-let del = require('del'); // delete
-//
-let fs = require('graceful-fs'); // JSON load
-let cache = require('gulp-cached'); // cache
-//
-//
-//
-//
-//
-//
-//
-//
-// server
-gulp.task('browser', function () {
+      //
+      // path
+      // - - - - - - - - - -
+      docs = '.',
+      //
+      distDir =  docs + '/dist',
+      srcDir =  docs + '/src',
+      //
+      srcAssetsDir = srcDir + '/assets',
+      distAssetsDir = distDir + '/assets',
+      //
+      srcPath = {
+        'imgPath': srcAssetsDir + '/images',
+        'sassPath': srcAssetsDir + '/sass',
+        'cssPath': srcAssetsDir + '/css',
+        'jsPath': srcAssetsDir + '/js'
+      },
+      distPath = {
+        'imgPath': distAssetsDir + '/images',
+        'sassPath': distAssetsDir + '/sass',
+        'cssPath': distAssetsDir + '/css',
+        'jsPath': distAssetsDir + '/js'
+      },
+
+      //
+      // common
+      // - - - - - - - - -
+      plumber = require('gulp-plumber'), // error escape
+      rename = require('gulp-rename'), // rename
+      sourcemaps = require('gulp-sourcemaps'), // sourcemap
+      gulpSequence = require('gulp-sequence'), // sequence
+      notify = require('gulp-notify'), // alert
+      watch = require("gulp-watch"),  // watch
+      del = require('del'), // delete
+      fs = require('graceful-fs'), // JSON load
+      cache = require('gulp-cached'), // cache
+
+      //
+      // CSS
+      // - - - - - - - - -
+      autoprefixer = require('gulp-autoprefixer'), // prefix
+      sass = require('gulp-compass'), // Sass compass
+      csscomb = require('gulp-csscomb'), // css
+      cssmin = require('gulp-cssmin'), // css min
+      frontnote = require('gulp-frontnote'), // style guide
+
+      //
+      // JavaScript
+      // - - - - - - - - -
+      uglify = require('gulp-uglify'), // js min
+      babel = require('gulp-babel'), // es6
+      concat = require('gulp-concat'), // concat ... order.JSON
+      eslint = require('gulp-eslint'), // eslint
+
+      //
+      // HTML
+      // - - - - - - - - -
+      ejs = require('gulp-ejs'), // ejs template
+      minifyHtml = require('gulp-minify-html'), // html min
+      browser = require('browser-sync'), // browser start
+
+      //
+      // image
+      // - - - - - - - - -
+      imagemin = require('gulp-imagemin'), // image min
+      pngquant = require('imagemin-pngquant');
+
+
+/**
+ * Start the server
+ *
+*/
+gulp.task('browser', () => {
   browser({ server: { baseDir: distDir + '/' } });
 });
-//
-//
-//
-// Sass
-/////////////////////////////
-gulp.task('sass', function () {
-  gulp.src(path.sassPath + '/**/*.scss')
+
+
+/**
+ * CSS task
+ *
+ * Convert Sass (SCSS) to CSS. (Compass)
+ * Generate a style guide.(frontnote)
+ * Execute autoprefixer.
+ * Format the order of CSS properties.
+ * Save it temporarily, compress it, rename it, resave it.
+ * Reload the browser.
+ *
+ * Sass(SCSS)をCSSに変換する。(compass)
+ * スタイルガイドを生成する。(frontnote)
+ * autoprefixerを実行する。
+ * CSSプロパティの並び順を整形する。
+ * 一時保存して、圧縮して名前を変更して、再保存。
+ * ブラウザを再起動する。
+ *
+*/
+gulp.task('sass', () => {
+  gulp.src(srcPath.sassPath + '/**/*.scss')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(sass({
       config_file: 'config.rb',
-      sass: path.sassPath,
-      css: path.cssPath,
-      image: path.imgPath
+      sass: srcPath.sassPath,
+      css: srcPath.cssPath,
+      image: srcPath.imgPath
     }))
-    //.pipe(frontnote({ out: path.cssPath }))
+    //.pipe(frontnote({ out: srcPath.cssPath }))
     .pipe(autoprefixer({
       browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4.4'],
       cascade: false
@@ -96,40 +124,53 @@ gulp.task('sass', function () {
     .pipe(browser.reload({ stream: true }))
     .pipe(notify('css task finished'));
 });
-//
-//
-//
-// js
-/////////////////////////////
-let jsJson = JSON.parse(fs.readFileSync(path.jsPath + '/order.json'));
-let jsList = [];
 
+
+/**
+ * JavaScript task
+ *
+ *
+ * Check the script with ESLint.
+ * Compile the ES 2015 notation to ES 5 with babel, save it after renaming.
+ * Join scripts in the order specified by JSON and save them.
+ * Compress the combined script, output the source map and save.
+* Reload the browser.
+ *
+ * ESLintでスクリプトをチェックする。
+ * babelでES2015記法をES5にコンパイルしてリネーム後、保存する。
+ * JSONで指定した順番にスクリプトを結合して保存する。
+ * 結合したスクリプトを圧縮し、ソースマップを出力して保存。
+ * ブラウザを再起動する。
+ *
+*/
+let jsJson = JSON.parse(fs.readFileSync(srcPath.jsPath + '/order.json')),
+    jsList = [],
+    cutLength;
 for (let i = 0; i < jsJson.order.length; i++) {
-  jsList[i] = path.jsPath + jsJson.order[i];
+  jsList[i] = srcPath.jsPath + jsJson.order[i];
 }
 //
-gulp.task('js.babel', function() {
-  return gulp.src(path.jsPath + '/**/*babel.js')
+gulp.task('js.babel', () => {
+  return gulp.src(srcPath.jsPath + '/**/*babel.js')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(eslint({useEslintrc: true}))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
     .pipe(babel())
-    .pipe(rename(function (path) {
-       let cutLength = path.basename.length - 6;
-       path.basename = path.basename.slice(0, cutLength);
+    .pipe(rename(function (Path) {
+       cutLength = Path.basename.length - 6;
+       Path.basename = Path.basename.slice(0, cutLength);
     }))
-    .pipe(gulp.dest(path.jsPath + '/babel/'))
+    .pipe(gulp.dest(srcPath.jsPath + '/babel/'))
 });
-gulp.task('js.concat', function() {
+gulp.task('js.concat', () => {
   return gulp.src(jsList.join(',').split(','))
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(concat('index.js'))
     .pipe(gulp.dest(distPath.jsPath + '/'))
-    .pipe(notify('js task finished'));
 });
 
-gulp.task('js.uglify', function() {
+gulp.task('js.uglify', () => {
   return gulp.src(distPath.jsPath + '/index.js')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(sourcemaps.init())
@@ -144,13 +185,22 @@ gulp.task('js.uglify', function() {
 gulp.task('js', function(callback) {
   gulpSequence('js.babel', 'js.concat', 'js.uglify')(callback)
 });
-//
-//
-//
-//
-// ejs html compail
-/////////////////////////////
-gulp.task('ejs.init', function(){
+
+
+/**
+ *
+ * Create common objects that can be used within ejs.
+ * Change the extension to html.
+ * Compress and save html.
+ * Reload the browser.
+ *
+ * ejs内で利用出来る共通のオブジェクトを作成。
+ * htmlに拡張子を変更する。
+ * htmlを圧縮して保存。
+ * ブラウザを再起動する。
+ *
+*/
+gulp.task('ejs.init', () => {
   return gulp.src([srcDir + '/**/*.ejs','!' + srcDir + '/**/*_*.ejs'])
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(ejs({data: JSON.parse(fs.readFileSync(srcDir + '/common/' + 'data.json'))}))
@@ -160,58 +210,58 @@ gulp.task('ejs.init', function(){
     .pipe(notify('html task finished'));
 });
 //
-gulp.task('ejs.reload', ['ejs.init'], function(){
+gulp.task('ejs.reload', ['ejs.init'], () => {
   return browser.reload();
 });
 //
 gulp.task('ejs', ['ejs.init', 'ejs.reload']);
-//
-//
-//
-// image
-/////////////////////////////
-//gulp.task('images', function () {
-//  gulp.src(path.imgPath + '/**/*.{png,jpg,gif,svg}')
-//    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>')}))
-//    //.pipe(cache( imagemin( [pngquant({quality: '60-80', speed: 1})] )))
-//    .pipe(gulp.dest(distPath.imgPath + '/'))
-//    .pipe(browser.reload({ stream: true }));
-//});
 
-gulp.task('images.min', function () {
-  return gulp.src(path.imgPath + '/**/*.{png,jpg,gif,svg}')
+
+/**
+ *
+ * Compress and save the image.
+ * Reload the browser.
+ *
+ * 画像を圧縮して保存。
+ * ブラウザを再起動する。
+ *
+*/
+gulp.task('images.min', () => {
+  return gulp.src(srcPath.imgPath + '/**/*.{png,jpg,gif,svg}')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>')}))
     //.pipe(cache( imagemin( [pngquant({quality: '60-80', speed: 1})] )))
     .pipe(gulp.dest(distPath.imgPath + '/'))
 });
 //
-gulp.task('images.reload', ['images.min'], function(){
+gulp.task('images.reload', ['images.min'], () => {
   return browser.reload();
 });
 //
 gulp.task('images', ['images.min', 'images.reload']);
-//
-//
-//
-// watch
-/////////////////////////////
-gulp.task('default', ['browser'], function () {
-  watch([path.jsPath + '/**/*.js', '!' + path.jsPath + '/babel/**/*.js'], function(event){ gulp.start(['js']) });
-  watch([path.sassPath + '/**/*.scss'], function(event){ gulp.start(['sass']) });
-  watch([srcDir + '/**/*.ejs'], function(event){ gulp.start(['ejs']) });
-  watch([path.imgPath + '/**/*.{png,jpg,gif,svg}'], function(event){ gulp.start(['images']) });
+
+
+/**
+ * dafault task
+ *
+*/
+gulp.task('default', ['browser'], () => {
+  watch([srcPath.jsPath + '/**/*.js', '!' + srcPath.jsPath + '/babel/**/*.js'], () => { gulp.start(['js']) });
+  watch([srcPath.sassPath + '/**/*.scss'], () => { gulp.start(['sass']) });
+  watch([srcDir + '/**/*.ejs'], () => { gulp.start(['ejs']) });
+  watch([srcPath.imgPath + '/**/*.{png,jpg,gif,svg}'], () => { gulp.start(['images']) });
 });
-//
-//
-//
-// release
-/////////////////////////////
-gulp.task('copy', function () {
+
+
+/**
+ * Clean up the file for release
+ *
+*/
+gulp.task('copy', () => {
   gulp.src(distDir + '/**/*')
     .pipe(gulp.dest('release/01'));
 });
 
-gulp.task('delete', function () {
+gulp.task('delete', () => {
   gulp.src('release/01')
     del(['release/**/*.LCK', 'release/**/*_notes', 'release/**/Templates/']);
 });
